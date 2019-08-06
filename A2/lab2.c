2f3c541c94b35f1f543b70b3cce36a0cd0b6aeb6
codewithjaysen <32177778+codewithjaysen@users.noreply.github.com>
2019-08-06 17:07:05 -0500

    Assignment 2

--- A2/lab2.c
+++ A2/lab2.c
@@ -1,9 +1,12 @@
 //Author: Jaysen Gonzales JRG301
 #include <stdio.h>
 #include <string.h> 
+#include<stdlib.h>    //For Exit()
+#include <unistd.h>   //For fork()
+#include <fcntl.h>    //For O_RDWR 
 
 
-main()
+int main()
 
 {
 
@@ -26,15 +29,15 @@ int argumentArray[20] = { 0 };
 
 while(1) {
 
-	inword = counter = pipes = pid = 0;
+    inword = counter = pipes = pid = 0;
 
-	p = buf;
-	m = 0;
-	continu=0;
+    p = buf;
+    m = 0;
+    continu=0;
 
     inputFlag = outFlag = 0;
 
-	printf( "\nshhh> ");
+    printf( "\nshhh> ");
 
 	while ( ( n = getchar() ) != '\n'  || continu ) {
 
@@ -49,11 +52,8 @@ while(1) {
 			} 
 
 		}
-
 		else if ( n == '\n' ) continu = 0;
-
 		else if ( n == '\\' && !inword ) continu = 1;
-
 		else {
 
 			if ( !inword ) {
@@ -69,9 +69,7 @@ while(1) {
 			else 
 
 				*p++ = n;
-
 		}
-
 	}	
 
 	*p++ = 0;
@@ -79,31 +77,37 @@ while(1) {
 	argv[m] = 0;
     
 	//No arguments EXIT
-	if ( strcmp(argv[0],"quit") == 0 ) exit (0);
+	if ( strcmp(argv[0],"exit") == 0 ) exit (0);
 
 
 	// Redirection Handler
-	while(argv[counter] =! 0)
+	while(argv[counter] != 0)
 	{
-		if (strcmp(argv[counter], "|") == 0) {
+		//pipe'|'
+        if (strcmp(argv[counter], "|") == 0) 
+        {
             argv[counter] = 0;
-            argumentContainer[pipes + 1] = counter + 1;
+            argumentArray[pipes + 1] = counter + 1;
             ++pipes;
         }
-		else if (strcmp(argv[counter], ">") == 0) {
+            //output direction
+        else if (strcmp(argv[counter], ">") == 0) 
+        {
             oPath = strdup(argv[counter + 1]); 
             argv[counter] = 0;
             argv[counter + 1] = 0;
             outFlag = 1;
-		}
-        else if (strcmp(argv[counter], "<") == 0) {
+        }
+            //input direction
+        else if (strcmp(argv[counter], "<") == 0) 
+        {
             iPath = strdup(argv[counter + 1]); 
             argv[counter] = 0;
             argv[counter + 1] = 0;
             inputFlag = 1;
         }
-        
-        else {
+        else 
+        {
             argumentArray[counter] = counter;
         }
 
@@ -111,21 +115,102 @@ while(1) {
 		 
 	}
 
+        
+	int loop;  //variable for 'for' loop, for loop declaration invalid
+ 
+	for (loop = 0; loop <= pipes; loop++){
+        if(pipes > 0 && loop != pipes)
+        {
+            pipe(rightPipe);
+        }
+        
+        //create new process
+        pid = fork();
 
+        // child creation was unsuccessful
+        if(pid < 0){
 
-	if ( fork() == 0 ){
+            printf("unsucccesful child creation");
+        }
+        
+        //child process was successful 
+ 	else if(pid == 0)
+        {
+            //creating input file
+            if ((loop == 0) && (inputFlag == 1))
+            {
+                int inputFile = open(iPath, O_RDONLY, 0400);
+                if (inputFile == -1)
+                {
+                    printf("input file failed\n");
+                    
+                }
+                close(0);
+                dup(inputFile);
+                close(inputFile);
+            }
+                    
+            if (pipes > 0)
+            {
+                if (loop == 0)
+                {
+                    close(1);
+                    dup(rightPipe[1]);
+                    close(rightPipe[1]);
+                    close(rightPipe[0]);
+                }
+                else if (loop < pipes)
+                {
+                    close(0);
+                    dup(leftPipe[0]);
+                    close(leftPipe[0]);
+                    close(leftPipe[1]);
+                    close(1);
+                    dup(rightPipe[1]);
+                    close(rightPipe[0]);
+                    close(rightPipe[1]);
+                }
+                else
+                {
+                    close(0);
+                    dup(leftPipe[0]);
+                    close(leftPipe[0]);
+                    close(leftPipe[1]);
+                }
+            }
+            //creaitng a output file 
+            if ((loop == pipes) && (outFlag == 1))
+            {
+                int outputFile = creat(oPath, 0700);
+                if (outputFile < 0)
+                {
+                    printf("output file failed.\n");
+                }
+                close(1);
+                dup(outputFile);
+                close(outputFile);
+            }
+            //command couldnt be read            
+            execvp(argv[argumentArray[loop]], &argv[argumentArray[loop]]);
+            printf("command failed !\n");
+        }
+        //parent process
+        else{             
+            if (loop > 0)
+            {
+                close(leftPipe[0]);
+                close(leftPipe[1]);
+            }
+            leftPipe[0] = rightPipe[0];
+            leftPipe[1] = rightPipe[1];
 
-		execvp( argv[0], argv );
+            wait(&status);
 
-		printf ( " didn't exec \n ");
-
-	}
-
-
-
-	wait(&status);
+                
+            }
+        }
+}
 	
-
 }//while 1 
 
-}//main
+
